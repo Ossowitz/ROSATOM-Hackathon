d95a9119fd42425dba52609dfb1f81f93fd9e684
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-07-27 00:45:50 +0300

    InitialCommit

--- .idea/script2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Устанавливаем nginx из папки /tmp
-sudo apt-get update
-sudo apt-get install -y nginx
-sudo cp /tmp/nginx.conf /etc/nginx/nginx.conf
-
-# Перезапускаем nginx
-sudo systemctl restart nginx.service
--- .idea/script3-2.groovy
+++
@@ -1,6 +0,0 @@
-import salt.modules.cmd
-
-String command = 'groovy /path/to/script.groovy'
-String output = salt.modules.cmd.run(command)
-
-println(output)
